d17cdd8e224751f1e8d5b943bf9c3ab7e186fc33
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-11-26 20:11:04 +0200

    initial commit with .gitignore file and config

---
+++ src/test/java/com/swe/dev/notificationhub/NotificationhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe.dev.notificationhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
